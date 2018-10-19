f4511751a58803331e17c1a2c26d9ab2be53c754
richardgao12345 <richard.gao12345@gmail.com>
2018-10-19 12:22:41 -0700

    Initial files and classes

---
+++ src/main/java/Transaction.java
@@ -0,0 +1,8 @@
+public class Transaction {
+    private int currency;
+    public Transaction(int amount) {
+        if (amount < 0) {
+
+        }
+    }
+}
---
+++ src/main/java/Wallet.java
@@ -0,0 +1,5 @@
+public class Wallet {
+    public static void main(String args[]) {
+        System.out.print("HI");
+    }
+}
